b4139ba5fffb5aa8aca6dcd036b66e0396373c36
Vineet Joshi <vineet0713@gmail.com>
2018-02-08 16:56:19 -0800

    Initial Commit

---
+++ Download Large ImagesTests/Download_Large_ImagesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Download_Large_ImagesTests.swift
+//  Download Large ImagesTests
+//
+//  Created by Vineet Joshi on 2/8/18.
+//  Copyright © 2018 Vineet Joshi. All rights reserved.
+//
+
+import XCTest
+@testable import Download_Large_Images
+
+class Download_Large_ImagesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
